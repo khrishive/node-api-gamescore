1fe7cab5697bdc6e540e06b0b688343eb5646329
oinilp <oinilp01@gmail.com>
2025-05-14 08:25:37 -0500

    no changes on src/inserts/insertFixtures.js, src/inserts/insertFixtures.js is the new file to add fixtures to db

--- src/inserts/insertFixtures.js
+++ src/inserts/insertFixtures.js
@@ -8,7 +8,8 @@ const dbConfig = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: cprocess.env.DB_NAME,
+    database: process.env.DB_NAME,
+    //port: process.env.DB_PORT,
 };
 
 async function fetchFixtures() {
