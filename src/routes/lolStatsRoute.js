3c9f6cd6a5474c292ed6a06d1735c2d7a535ca55
oinilp <oinilp01@gmail.com>
2025-10-29 07:59:13 -0500

    added lol pick ban endpoint

--- src/routes/lolStatsRoute.js
+++ src/routes/lolStatsRoute.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { lolMapStats } from '../controllers/controllerMapResultsLol.js';
+import { lolPickBan } from '../controllers/controllerPickBanLol.js';
 
 const router = express.Router();
 
@@ -7,6 +8,17 @@ router.get('/:fixtureId/mapStats', async (req, res) => {
   try {
     const fixtureId = req.params.fixtureId;
     const data = await lolMapStats(fixtureId);
+    if (!data) return res.status(404).json({ error: 'Fixture not found' });
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+router.get('/:fixtureId/pickBan', async (req, res) => {
+  try {
+    const fixtureId = req.params.fixtureId;
+    const data = await lolPickBan(fixtureId);
     if (!data) return res.status(404).json({ error: 'Fixture not found' });
     res.json(data);
   } catch (err) {
